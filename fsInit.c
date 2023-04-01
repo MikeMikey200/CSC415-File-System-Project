a194bfeff2126e1e688a50dd330ac112a91cdf47
Minh <minhmike200@gmail.com>
2023-03-31 17:37:44 -0700

    vcb init

--- fsInit.c
+++ fsInit.c
@@ -24,12 +24,38 @@
 #include "fsLow.h"
 #include "mfs.h"
 
+// contains volume details
+typedef struct vcb {
+	// no need location for vcb itself, it is at 0 in the LBA
+	int signature; // an integer of a magic number or a string like ELF 
+	unsigned int locationFreespace; // location of block # for free space  
+	unsigned int locationRootDir; // location of block # for root dir       
+	unsigned int blockNum; // num of block in the volume
+	unsigned int blockNumFree; // num of free blocks in the freespace
+	unsigned int blockSize; // size of each blocks typically 512
+} vcb;
 
 int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	{
+	
 	printf ("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 	/* TODO: Add any code you need to initialize your file system. */
+	int vcbSize = sizeof(vcb); // size of vcb
+	int vcbBlock = (vcbSize + MINBLOCKSIZE - 1) / MINBLOCKSIZE; // location of vcb
+	vcb *fsvcb = malloc(vcbSize);
 
+	//initializing
+	fsvcb->signature = 1234; // please change to a value that we want
+	fsvcb->locationFreespace = vcbBlock + 1; // temporary value
+	fsvcb->locationRootDir = vcbBlock + 2; // temporary value
+	fsvcb->blockNum = numberOfBlocks; // num of block in LBA
+	fsvcb->blockNumFree = numberOfBlocks - (vcbBlock + 2); // temporary value
+	fsvcb->blockSize = blockSize; // size of each block
+
+	LBAwrite((void *)fsvcb, 1, 1); // size of vcb usually should be smaller MINBLOCKSIZE to be fit in a single block 
+			       // so we don't have to worry about vcb overflowing to multiple blocks
+
+	free(fsvcb);
 	return 0;
 	}
 	
