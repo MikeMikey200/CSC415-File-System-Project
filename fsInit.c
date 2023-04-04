4448887063c1693593486ebe509a892ae9cb72e2
Minh <minhmike200@gmail.com>
2023-04-04 01:02:23 -0700

    freespace functions allocation and release implemented

--- fsInit.c
+++ fsInit.c
@@ -30,83 +30,128 @@
 typedef struct vcb {
 	// no need location for vcb itself, it is at 0 in the LBA
 	int signature; // an integer of a magic number or a string like ELF 
-	unsigned int locationFreespace; // location of block # for free space  
-	unsigned int locationRootDir; // location of block # for root dir       
-	unsigned int blockNum; // num of block in the volume
-	unsigned int blockNumFree; // num of free blocks in the freespace
-	unsigned int blockSize; // size of each blocks typically 512
+	uint64_t locationFreespace; // location of block # for free space  
+	uint64_t locationRootDir; // location of block # for root dir       
+	uint64_t blockNum; // num of block in the volume
+	uint64_t blockNumFree; // num of free blocks in the freespace
+	uint64_t blockSize; // size of each blocks typically 512
 } vcb;
 
+// contains FAT implementation
 typedef struct fat {
-	unsigned int used; // indicate 0 is free, 1 is used
-	unsigned int next; // indicate next location greater than -1 is location, -1 is EOF
+	uint64_t used; // indicate 0 is free, 1 is used
+	uint64_t next; // indicate next location > 0 is location, 0 is EOF
 } fat;
 
-// returning block location if there is free block, otherwise -1
-int freespaceFindFreeBlock(fat *freespace, uint64_t numberOfBlocks) {
-	for(unsigned int i = 0; i < numberOfBlocks; i++) {
-		if(freespace[i].used == 0) {
+// this is an allocation function for freespace using FAT method
+// startLocation indicate if not in freespace 0, if exist in freespace > 0
+// return an index, otherwise 0 indicate an error
+uint64_t freespaceFindFreeBlock(fat *freespace, uint64_t numberOfBlocks, uint64_t startLocation) {
+	uint64_t i = 0;
+
+	// mean there is an allocated file here already recursively go to freespace.next until reaches -1
+	if (startLocation > 0){
+		while (freespace[startLocation].next != 0) {
+			startLocation = freespace[startLocation].next;
+			if (startLocation == freespace[startLocation].next)
+				return 0;
+		}
+		i = startLocation;
+	}
+	
+	for(; i < numberOfBlocks; i++) {
+		if (freespace[i].used == 0) {
+			if (startLocation != 0){
+				freespace[startLocation].next = i;
+			}
 			freespace[i].used = 1;
 			return i;
 		}
 	}
-	return -1;
+
+	return 0;
+}
+
+// this is a release function for freespace using FAT method
+// startLocation indicate starting block# to be free, has to be > -1
+// return 1 indicate good, return 0 indicate error for startLocation
+uint64_t freespaceReleaseBlock(fat *freespace, uint64_t numberOfBlocks, uint64_t startLocation){
+	uint64_t i;
+	uint64_t start;
+
+	// must have a start location
+	if (startLocation == 0) {
+		return 0;
+	}
+
+	while(freespace[startLocation].next != 0){
+		i = startLocation;
+		freespace[startLocation].used = 0;
+		startLocation = freespace[startLocation].next;
+		freespace[i].next = 0;
+	}
+
+	freespace[startLocation].used = 0;
+
+	return 1;
 }
 
 int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	{
 	printf ("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 	/* TODO: Add any code you need to initialize your file system. */
-	int vcbSize = sizeof(vcb); // size of vcb
-	int vcbBlock = (vcbSize + blockSize - 1) / blockSize; // num of blocks of vcb
+	uint64_t vcbSize = sizeof(vcb); // size of vcb
+	uint64_t vcbBlock = (vcbSize + blockSize - 1) / blockSize; // num of blocks of vcb
 	vcb *fsvcb = malloc(vcbSize);
+
 	LBAread(fsvcb, vcbBlock, 0);
+
 	// this is to check whether vcb is already init so we don't override disk
 	if(fsvcb->signature == SIGNATURE){
 		//return 0; uncomment this whenever rootDir init is finished
 	}
 
-	int fatSize = sizeof(fat); // size of fat
-	int fatBlock = (fatSize * numberOfBlocks + blockSize - 1) / blockSize; // num of blocks of fat
+	uint64_t fatSize = sizeof(fat); // size of fat
+	uint64_t fatBlock = (fatSize * numberOfBlocks + blockSize - 1) / blockSize; // num of blocks of fat
 	fat *freespace = malloc(fatSize * numberOfBlocks);
 
 	// initializing freespace
-	for(unsigned int i = 0; i < numberOfBlocks; i++){
+	for(uint64_t i = 0; i < numberOfBlocks; i++){
 		freespace[i].used = 0;
-		freespace[i].next = -1;
+		freespace[i].next = 0;
 	}
 
-	int rootBlock = 1; // please fill this one with rootDir block size
+	uint64_t rootBlock = 1; // please fill this one with rootDir block size
 
 	/* TODO:
 	fill in intialization of rootDir and making of rootDir here
 	*/
 
-	int totalBlock = vcbBlock + fatBlock + rootBlock;
+	uint64_t totalBlock = vcbBlock + fatBlock + rootBlock;
 
 	// mark vcb on freespace map as used
-	for(unsigned int i = 0; i < vcbBlock; i++){
+	for(uint64_t i = 0; i < vcbBlock; i++){
 		freespace[i].used = 1;
 		freespace[i].next = i + 1;
 	}
 
-	freespace[vcbBlock - 1].next = -1;
+	freespace[vcbBlock - 1].next = 0;
 
 	// mark freespace on freespace map as used
-	for(unsigned int i = vcbBlock; i < vcbBlock + fatBlock; i++){
+	for(uint64_t i = vcbBlock; i < vcbBlock + fatBlock; i++){
 		freespace[i].used = 1;
 		freespace[i].next = i + 1;
 	}
 
-	freespace[vcbBlock + fatBlock - 1].next = -1;
+	freespace[vcbBlock + fatBlock - 1].next = 0;
 
 	// mark rootDir on freespace map as used
-	for(unsigned int i = vcbBlock + fatBlock; i < totalBlock; i++){
+	for(uint64_t i = vcbBlock + fatBlock; i < totalBlock; i++){
 		freespace[i].used = 1;
 		freespace[i].next = i + 1;
 	}
 
-	freespace[totalBlock - 1].next = -1;
+	freespace[totalBlock - 1].next = 0;
 	
 	// initializing vcb
 	fsvcb->signature = SIGNATURE; // refer to #define above
@@ -116,14 +161,38 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	fsvcb->blockNumFree = numberOfBlocks - totalBlock; 
 	fsvcb->blockSize = blockSize; // size of each block
 
+	// writing to disk
 	LBAwrite((void *)fsvcb, vcbBlock, 0); // size of vcb usually should be smaller blockSize to be fit in a single block 
 			       	              // so we don't have to worry about vcb overflowing to multiple blocks
 	LBAwrite((void *)freespace, fatBlock, vcbBlock); // start from vcbBlock
 	//LBAwrite((void *)rootDir, rootBlock, vcbBlock + fatBlock); uncomment this when rootDir is finished
+
+	// example of how to use freespaceFindFreeBlock(fat *freespace, uint64_t numberOfBlocks, uint64_t startLocation)
+	printf("locationFreespace: %ld\n", fsvcb->locationFreespace);
+	printf("locationRootDir: %ld\n", fsvcb->locationRootDir);
+	uint64_t num;
+	num = freespaceFindFreeBlock(freespace, numberOfBlocks, 0);
+	printf("num: %ld\n", num);
+	printf("num %ld free.next: %ld\n", num - 1, freespace[num - 1].next);
+	num = freespaceFindFreeBlock(freespace, numberOfBlocks, num);
+	printf("num: %ld\n", num);
+	printf("num %ld free.next: %ld\n", num - 1, freespace[num - 1].next);
+	num = freespaceFindFreeBlock(freespace, numberOfBlocks, num);
+	printf("num: %ld\n", num);
+	printf("num %ld free.next: %ld\n", num - 1, freespace[num - 1].next);
+
+	// example of how to use freespaceReleaseBlock(fat *freespace, uint64_t numberOfBlocks, uint64_t startLocation)
+	freespaceReleaseBlock(freespace, numberOfBlocks, num - 2);
+	printf("num %ld free.used: %ld\n", num - 2, freespace[num - 2].used);
+	printf("num %ld free.next: %ld\n", num - 2, freespace[num - 2].next);	
+	printf("num %ld free.used: %ld\n", num - 1, freespace[num - 1].used);
+	printf("num %ld free.next: %ld\n", num - 1, freespace[num - 1].next);
+	printf("num %ld free.used: %ld\n", num, freespace[num].used);
+	printf("num %ld free.next: %ld\n", num, freespace[num].next);
 	
+	// free up resources
 	free(fsvcb);
 	free(freespace);
-	
 	return 0;
 	}
 	
