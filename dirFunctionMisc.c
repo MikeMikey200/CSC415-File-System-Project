39bc2736f0e89b384eab68f25b4b4a65242abd38
Minh <minhmike200@gmail.com>
2023-04-16 13:51:00 -0700

    fix isDir and isFile

--- dirFunctionMisc.c
+++ dirFunctionMisc.c
@@ -48,21 +48,28 @@ char *fs_getcwd(char *pathname, size_t size) {
     pathname[0] = '\0';
     printf("here\n");
 
-    char *str = malloc(sizeof(pathname));
-    str[0] = '\0';
+    char str[size];
+    str[0] = '\0'; 
 
     char *name;
     unsigned int location;
 
-    
+    int index = 0;
     while(dir[0].location != rootDir[0].location) {
         location = dir[0].location;
         LBAread(dir, dir[1].size / fsvcb->blockSize, dir[1].location);
         name = dirFindName(dir, location);
         
         strcat(pathname, "\\");
+        index++;
         strcat(pathname, name);
+        index += strlen(name);
         strcat(pathname, str);
+
+        if (index >= size) {
+            free(dir);
+            return NULL;
+        }
 
         strcpy(str, pathname);
         strcpy(pathname, "");
@@ -71,38 +78,38 @@ char *fs_getcwd(char *pathname, size_t size) {
     strcpy(pathname, str);
 
     free(dir);
-    free(str);
     return pathname;
 }
 
 int fs_isDir(char * pathname) {
-    dirEntry *dir = malloc(MAXENTRIES * sizeof(dirEntry));
+    dirEntry *dir = malloc(BLOCK(sizeof(dirEntry), MAXENTRIES, fsvcb->blockSize) * fsvcb->blockSize);
     if (dir == NULL) {
         return -1;
     }
 
     // check if pathname is a directory
-    if (parsePath(pathname, rootDir, dir) != -1 ) {
+    int index = parsePath(pathname, rootDir, dir);
+    if (index == -1 ) {
+        free(dir);
+        return -1;
+    }
+
+    if (dir[index].type == 0) {
         free(dir);
         return 1;
     }
-
     else {
         free(dir);
         return 0;
     }
-
 }
 
-/*
 int fs_isFile(char * filename) {
-    if (!fs_isDir(pathname)) {
-        return 1;
+    for(int i = 0; i < currentwd->size / fsvcb->blockSize; i++) {
+        if (strcmp(currentwd[i].name, filename) == 0) {
+            return 1;
+        }
     }
 
-    else {
-        return 0;
-    }
-
+    return 0;
 }
-*/
